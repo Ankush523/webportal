0abfa8c22d8d739bf15c25199a677e66a5436683
Ankush Dutta <ankush.ad050203@gmail.com>
2022-12-13 12:58:24 +0530

    Patents page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar';
-import Login from './pages/Login';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
 import Profile from './pages/Profile';
 import Papers from './pages/Papers';
+import Patents from './pages/Patents';
+import NewData from './pages/NewData';
 
 function App() {
   return (
@@ -14,6 +14,8 @@ function App() {
           <Route path="/" element={<Login/>}/>
           <Route path="/profile" element={<Profile/>}/>
           <Route path="/papers" element={<Papers/>}/>
+          <Route path="/patents" element={<Patents/>}/>
+          <Route path="/newdata" element={<NewData/>}/>
         </Routes>
       </BrowserRouter>
     </div>
